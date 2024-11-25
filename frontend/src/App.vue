cd4ea8044dd667bd17c2db812f6201b5a78f4d15
l1ghth4t <pirogov30002@gmail.com>
2024-11-26 01:53:23 +0300

    add: base frontend project

---
+++ frontend/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <div class="container py-8">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elit.
+    <Button>Click me</Button>
+  </div>
+</template>
