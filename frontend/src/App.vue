d7ae52f82c2207aeb0f7fadb82e8b6a909c101bc
l1ghth4t <pirogov30002@gmail.com>
2024-11-26 09:30:20 +0300

    victory: routing with uikit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,11 @@
-<script setup lang="ts">
-import { Button } from '@/components/ui/button'
-</script>
-
 <template>
-  <div class="container py-8">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elit.
-    <Button>Click me</Button>
+  <div id="app">
+    <router-view></router-view>
   </div>
 </template>
+
+<script lang="ts">
+export default {
+  name: 'App',
+};
+</script>
