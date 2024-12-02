e9d465fe25766e5acfe0b48b5e098ef8683c9e5d
l1ghth4t <pirogov30002@gmail.com>
2024-12-02 05:50:46 +0300

    add: CQRS result adapter

---
+++ backend/core/cqrs/commands/agent_handle_context.py
@@ -0,0 +1,36 @@
+"""
+Команда для обработки контекста выполнения запроса, присылаемого агентом.
+"""
+
+import json
+import base64
+
+
+class CommandHandleContext:
+    """
+    Команда для обработки контекста выполнения запроса, присылаемого агентом.
+    """
+
+    def __init__(self, project_id, request_base_64, control_flow_base_64, response_base_64):
+        """
+        Конструктор команды.
+        :param project_id: id анализируемого приложения.
+        :param context_base_64: контекст выполнения запроса в формате base64.
+        """
+        self.project_id = project_id
+        self.request_base_64 = request_base_64
+        self.control_flow_base_64 = control_flow_base_64
+        self.response_base_64 = response_base_64
+
+    def execute(self):
+        """
+        Выполнение команды.
+        """
+
+        try:
+            json_request = base64.b64decode(self.request_base_64).decode("utf-8")
+            json_control_flow = base64.b64decode(self.control_flow_base_64).decode("utf-8")
+            json_response = base64.b64decode(self.response_base_64).decode("utf-8")
+
+        except (KeyError, ValueError, json.JSONDecodeError) as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
