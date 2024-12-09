9220b01ffeb66b7122929672674b7d6db56c29dc
l1ghth4t <pirogov30002@gmail.com>
2024-12-09 11:52:56 +0300

    update: linting backend

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,7 @@
+"""
+Тестовый обработчик для графа.
+"""
+
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
