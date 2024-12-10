47180543918abaf4dbc95c6c2cacc8a58104e109
l1ghth4t <pirogov30002@gmail.com>
2024-12-10 17:28:57 +0300

    update: release pipeline and docker-compose config

--- backend/conf/settings/prod.py
+++ backend/conf/settings/prod.py
@@ -5,9 +5,8 @@
 import os
 from os import environ
 
-from django.core.management.utils import get_random_secret_key
-
 from conf.settings.base import *  # pylint: disable=unused-wildcard-import
+from django.core.management.utils import get_random_secret_key
 
 SECRET_KEY = get_random_secret_key()
 
